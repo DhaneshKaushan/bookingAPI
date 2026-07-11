4ede26d3b0c76fc824cf15c03e052b800269812c
Dhanesh Kaushan <dhaneshkaushan@gmail.com>
2026-07-11 10:21:06 +0530

    initial project and  config Database conection

---
+++ src/main.ts
@@ -0,0 +1,35 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Booking API')
+    .setDescription('Booking Management API')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT || 3000);
+
+  console.log(
+    `Server running on http://localhost:${process.env.PORT || 3000}`,
+  );
+}
+
+bootstrap();
